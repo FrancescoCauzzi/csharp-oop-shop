56006df2ee38c11cad2cb9c01126182bc0f87778
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-09-08 16:46:10 +0200

    feat: style improvements added

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Product product = new Product("Hairdryer", "An appliance used mainly to dry hair", 25.99M, 0.22M);
-            Console.WriteLine($"The product name is {product.Name}");
+            Product product = new Product("Hairdryer", "An appliance used mainly to dry hair", 32M, 0.22M);
+            Console.WriteLine($"The product name is: {product.Name}");
             Console.WriteLine($"Description: {product.Description}");
-            Console.WriteLine($"The product code is {product.ProductCode}");
+            Console.WriteLine($"The product code is: {product.ProductCode}");
             Console.WriteLine($"The product net price is: {product.Price}");
             Console.WriteLine($"The product gross price is: {product.GetGrossPrice()}");
-            Console.WriteLine($"The extended name of the product is {product.GetFullName()}");
+            Console.WriteLine($"The extended name of the product is: {product.GetFullName()}");
 
 
 
