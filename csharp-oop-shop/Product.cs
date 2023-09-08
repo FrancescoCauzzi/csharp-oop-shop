78b616ee2bcfba3685e80b02fc35c8a8fcec8b89
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-09-08 16:41:27 +0200

    feat: base exercise, done

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    /*
+        Un prodotto è caratterizzato da:
+        codice (numero intero)
+        nome
+        descrizione
+        prezzo
+        iva
+        */
+
+    public class Product
+    {
+        // private fields
+        private int _productCode;
+        private string _name;
+
+        private string _description;
+
+        private decimal _price;
+
+        private decimal _vat;
+
+        // constructor
+
+        public Product(string name, string description, decimal price, decimal vat)
+        {
+            Random random = new Random();
+            this._productCode = random.Next(10000000, 100000000);
+            this._name = name;
+            this._description = description;
+            this._price = price;
+            this._vat = vat;
+        }
+
+        // Properties
+        public int ProductCode
+        {
+            get { return _productCode; }
+            set { _productCode = value; }
+
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public decimal Price
+        {
+            // this get method in the property exposes the basic price
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public decimal Vat
+        {
+            get { return _vat; }
+            set { _vat = value; }
+        }
+
+        public decimal GetGrossPrice()
+        {
+            decimal grossPrice = this.Price + (this.Price * this.Vat); ;
+            return grossPrice;
+        }
+
+        public string GetFullName()
+        {
+            return $"{this.ProductCode}{this.Name}";
+        }
+
+
+
+
+    }
+}
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            // OOP-Shop
+            Product product = new Product("Hairdryer", "An appliance used mainly to dry hair", 25M, 0.22M);
+            Console.WriteLine($"The product name is {product.Name}");
+            Console.WriteLine($"Description: {product.Description}");
+            Console.WriteLine($"The product code is {product.ProductCode}");
+            Console.WriteLine($"The product net price is: {product.Price}");
+            Console.WriteLine($"The product gross price is: {product.GetGrossPrice()}");
+            Console.WriteLine($"The extended name of the product is {product.GetFullName()}");
+
+
+
+
         }
+
+
+
+
+
+
+
     }
 }
